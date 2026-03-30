159616a4b657d9bd18e1292aaf949ab9f29675ed
nick-misomi <mwikasom@gmail.com>
2026-03-30 12:15:54 +0300

    Fix Vite config and entry point

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, './'), // root is client folder
+  base: '/',
   build: {
-    outDir: 'dist', // will generate client/dist
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html')
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src') // optional if you want '@/components/...'
-    }
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
